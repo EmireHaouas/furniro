98f8284e3da498fb904a343cc7ed96b220412ce8
EmireHaouas <emire.haouas100@gmail.com>
2024-11-25 01:23:51 +0100

    added banner

--- furniro/src/Components/Main.jsx
+++ furniro/src/Components/Main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import '../Main.css';
+import background_banner from '../assets/imgs/background_banner.png';
 
 const Main = () =>{
     return(
         <section className="banner">
-            <h1>hhdz</h1>
+         {/* <img className="banner_Img" alt="Banner img of furnitures" src={background_banner}/>*/}
+          <div className="stick">
+            <p className="p_Stick">New Arrival</p>
+            <h1 className="h1_Stick">Discover Our<br></br> New Collection</h1>
+            <p className="p_Stick2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut<br></br> 
+            elit tellus, luctus nec ullamcorper mattis.</p>
+            <button className="stick_Button">Buy Now</button>
+          </div>
         </section>
         
     )

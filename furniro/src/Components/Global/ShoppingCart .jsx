001c850d9b97c57c3ae49a900cf1082b9e1387b5
EmireHaouas <emire.haouas100@gmail.com>
2024-11-29 01:58:37 +0100

    added design on shoping cart

--- furniro/src/Components/Global/ShoppingCart .jsx
+++ furniro/src/Components/Global/ShoppingCart .jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"; 
 import './Cart.css';
 import cart_Icon from'../../assets/imgs/cart_Icon.png';
+import close_Icon from '../../assets/imgs/close_Icon.png';
 
 const ShoppingCart = () => {
     const [isCartOpen, setIsCartOpen] = useState(false);
@@ -25,16 +26,27 @@ const ShoppingCart = () => {
 
             {/* Barre latérale (panier) */}
             <div id="shopping-cart" className={isCartOpen ? "active" : ""}>
+                <div className="row1">
                 <button className="close-btn" onClick={toggleCart}>
-                    ×
+                    <img src={close_Icon} alt="Close Icon" className="close_icon" />  {/* Icône de fermeture */}
                 </button>
-                <h3>Shopping Cart</h3>
+                <div className="title_Hr">
+                <h3 className="title_Cart">Shopping Cart</h3>
+                <hr></hr>
+                </div>
+                </div>
                 <ul>
                     <li>Asgaard Sofa - Rs. 250,000.00</li>
                     <li>Casaliving Wood - Rs. 270,000.00</li>
                 </ul>
-                <div className="subtotal">Subtotal: Rs. 520,000.00</div>
+                <div className="subtotal"><span className="sub_Cart">Subtotal: </span>  <span className="price_Cart">$. 520,000.00</span></div>
+                <hr className="hr2"></hr>
+                <div className="button_Cart">
+                <button className="cart_Button">Cart</button>
+                <button className="checkout_Button">Checkout</button>
+                </div>
             </div>
+            
         </div>
     );
 };

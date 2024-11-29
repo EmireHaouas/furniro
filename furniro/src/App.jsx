d10d42049bff41431f0a89375fa2cd0e933e5d84
EmireHaouas <emire.haouas100@gmail.com>
2024-11-29 05:56:56 +0100

    added react router

--- furniro/src/App.jsx
+++ furniro/src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Header from './Components/Global/Header';
-import Main from './Components/Home/Main';
 import Footer from './Components/Global/Footer';
 import Home from './Components/Home/Home';
 import Shop from './Components/Shop/Shop';
-import ShoppingCart  from './Components/Global/ShoppingCart ';
-const App=() => {
 
-
-  return (
-    <div>
-    <Shop  />
-    </div>
-    
-  )
-}
+const App = () => {
+    return (
+        <Router>
+            
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+            </Routes>
+            
+        </Router>
+    );
+};
 
 export default App;
--- furniro/src/Components/Global/Header.jsx
+++ furniro/src/Components/Global/Header.jsx
@@ -1,38 +1,35 @@
 import React from "react";
 import './Header.css';
+import { Link } from "react-router-dom";
 import logo from "../../assets/imgs/logo.png";
-import user_Icon from'../../assets/imgs/user_Icon.png';
-import cart_Icon from'../../assets/imgs/cart_Icon.png';
-import heart_Icon from'../../assets/imgs/heart_Icon.png';
-import search_Icon from'../../assets/imgs/search_Icon.png';
+import user_Icon from '../../assets/imgs/user_Icon.png';
+import heart_Icon from '../../assets/imgs/heart_Icon.png';
+import search_Icon from '../../assets/imgs/search_Icon.png';
 import ShoppingCart from './ShoppingCart ';
 
-
-
-const Header = () =>{
-    return(
+const Header = () => {
+    return (
         <header className="header">
             <div className="test">
-            <div className="logo_Pack">
-                <img className="logo" alt="logo" src={logo}/>
-                <p className="logo_Name">Furniro</p>
-            </div>
-            <nav className="nav_Bar" >
-                <a href="" className="home_Nav">Home</a>
-                <a href="" className="shop_Nav">Shop</a>
-                <a href="" className="about_Nav">About</a>
-                <a href="" className="contact_Nav">Contact</a>
-
-            </nav>
+                <div className="logo_Pack">
+                    <img className="logo" alt="logo" src={logo} />
+                    <p className="logo_Name">Furniro</p>
+                </div>
+                <nav className="nav_Bar">
+                    <Link to="/" className="home_Nav">Home</Link>
+                    <Link to="/shop" className="shop_Nav">Shop</Link>
+                    <Link to="/about" className="about_Nav">About</Link>
+                    <Link to="/contact" className="contact_Nav">Contact</Link>
+                </nav>
             </div>
             <div className="icons_Nav">
-                 <img className="icons" src={user_Icon} alt=""  />
-                 <img className="icons" src={search_Icon} alt=""  />
-                 <img className="icons" src={heart_Icon} alt=""  />
-                 <ShoppingCart />
-           </div> 
-
+                <img className="icons" src={user_Icon} alt="User Icon" />
+                <img className="icons" src={search_Icon} alt="Search Icon" />
+                <img className="icons" src={heart_Icon} alt="Heart Icon" />
+                <ShoppingCart />
+            </div>
         </header>
-    )
-}
+    );
+};
+
 export default Header;

71a78ce8aca06e16db3ee7d535058916df6a1f3e
EmireHaouas <emire.haouas100@gmail.com>
2024-12-08 10:40:36 +0100

    change stufs in checkout page

--- furniro/src/Components/Checkout/Checkout.jsx
+++ furniro/src/Components/Checkout/Checkout.jsx
@@ -3,6 +3,7 @@ import "./Checkout.css";
 import bannercheckout from "../../assets/imgs/bannercheckout.png";
 import Warranty from "../Global/Warranty";
 import { useCart } from "../Global/Cartcontext"; // Importer le contexte
+import Countries from '../../Data/Countries';
 
 const Checkout = () => {
   const { cart } = useCart(); // Récupérer les produits du panier
@@ -13,7 +14,7 @@ const Checkout = () => {
   }
 
   return (
-    <div className="checkout-container">
+    <div className="checkout_Container">
       {/* Bannière */}
       <img
         src={bannercheckout}
@@ -22,43 +23,74 @@ const Checkout = () => {
       />
 
       {/* Détails de facturation */}
-      <h1>Billing details</h1>
-      <div className="checkout-content">
+      <h1 className="h1_Checkout">Billing details</h1>
+      <div className="checkout_Content">
         {/* Formulaire de facturation */}
-        <div className="billing-details">
+        <div className="billing_Details">
           <form>
             <div className="form-row">
-              <input type="text" placeholder="First Name" />
-              <input type="text" placeholder="Last Name" />
+              <div className="firstname_Check">
+              <p>First Name</p>
+              <input className="nameto_Check" type="text" required/>
+              </div>
+              <div className="lastname_Check">
+                <p>Last Name</p>
+              <input className="nameto_Check2" type="text"  required/>
+              </div>
             </div>
-            <input type="text" placeholder="Company Name (Optional)" />
-            <select>
-              <option>Country / Region</option>
-              <option>Sri Lanka</option>
+            <div className="companyname_Check">
+              <p>Company Name (Optional)</p>
+            <input className="data_Check" type="text" />
+            </div>
+            <div className="country_Check">
+            <p>Country / Region</p>
+            <select className="data_Check" required>
+              <option>Japan</option>
+              {Countries.map((country, index) => (
+                <option key={index}>{country}</option>
+              ))}
             </select>
-            <input type="text" placeholder="Street address" />
-            <input type="text" placeholder="Town / City" />
-            <select>
-              <option>Province</option>
-              <option>Western Province</option>
-            </select>
-            <input type="text" placeholder="ZIP code" />
-            <input type="text" placeholder="Phone" />
-            <input type="email" placeholder="Email address" />
-            <textarea placeholder="Additional Information"></textarea>
+            </div>
+            <div className="streetadress_Check">
+              <p>Street Adress</p>
+            <input className="data_Check" type="text"  required/>
+            </div>
+            <div className="city_Check">
+              <p>Town / City</p>
+            <input  className="data_Check" type="text"  required/>
+            </div>
+            
+            <div className="province_Check">
+              <p>Province (Optional)</p>
+            <input  className="data_Check" type="text" />
+            </div>
+            <div className="zipcode_Check">
+              <p>ZIP Code</p>
+            <input className="data_Check" type="text"  required/>
+            </div>
+            <div className="phone_Check">
+              <p>Phone</p>
+            <input className="data_Check" type="text" required/>
+            </div>
+            <div className="email_Check">
+              <p>Email Address</p>
+            <input className="data_Check" type="email" required/>
+            </div>
+            
+            <textarea className="textarea_Checkout" placeholder="Additional Information"></textarea>
           </form>
         </div>
 
         {/* Résumé de la commande */}
-        <div className="order-summary">
-          <h2>Order Summary</h2>
+        <div className="order_Summary">
+          
           <div className="items_Cart">
             <div className="rowcart_Check header">
               <div className="column_Check">
-                <p>Product</p>
+                <p className="p_Checkcolum">Product</p>
               </div>
               <div className="column_Check2">
-                <p>Subtotal</p>
+                <p className="p_Checkcolum2">Subtotal</p>
               </div>
             </div>
 
@@ -81,6 +113,10 @@ const Checkout = () => {
             <h3>Payment Methods</h3>
             <label>
               <input type="radio" name="payment" />
+              Credit Card
+            </label>
+            <label>
+              <input type="radio" name="payment" />
               Direct Bank Transfer
             </label>
             <label>
@@ -92,7 +128,7 @@ const Checkout = () => {
         </div>
       </div>
 
-      {/* Garantie */}
+     
       <Warranty />
     </div>
   );
